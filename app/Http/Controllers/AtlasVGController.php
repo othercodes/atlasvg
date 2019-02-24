94fc4a83acf7d89103e60b9780cb317bda87e5cc
Unay Santisteban <usantisteban@othercode.es>
2019-02-24 20:13:04 +0100

    Testing svg manipulating.

--- app/Http/Controllers/AtlasVGController.php
+++ app/Http/Controllers/AtlasVGController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use SVG\Nodes\Shapes\SVGCircle;
 use SVG\SVG;
 
 class AtlasVGController extends Controller
@@ -12,6 +13,15 @@ class AtlasVGController extends Controller
         $map = SVG::fromString($this->getMap('sample.svg'));
 
         // need some magic to process the svg
+
+        // get the dom document from the svg
+        $doc = $map->getDocument();
+
+        // test circle with radius 20 and green border, center at (50, 50)
+        $doc->addChild((new SVGCircle(50, 50, 20))
+            ->setStyle('fill', 'none')
+            ->setStyle('stroke', '#0F0')
+            ->setStyle('stroke-width', '2px'));
 
         return view('index', ['map' => $map]);
     }
