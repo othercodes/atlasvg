035a818c537c31a7175f22f7297279b6331ec13f
Unay Santisteban <davidu@softcom.com>
2019-03-15 17:55:19 +0100

    Show command done.

--- app/Models/Building.php
+++ app/Models/Building.php
@@ -77,7 +77,11 @@ class Building extends Model
      */
     public function getSvgAttribute($svg)
     {
-        return new SimpleXMLIterator($svg, LIBXML_COMPACT);
+        if (isset($svg)) {
+            return new SimpleXMLIterator($svg, LIBXML_COMPACT);
+        }
+
+        return null;
     }
 
     /**
@@ -108,10 +112,12 @@ class Building extends Model
      * Get surroundings map path
      * @return string
      */
-    public function getMapAttribute()
+    public function getMapAttribute($svg)
     {
         $path = resource_path("maps/b{$this->id}.surroundings.svg");
-        $this->svg->saveXML($path);
+        if (isset($this->svg)) {
+            $this->svg->saveXML($path);
+        }
 
         return $this->attributes['map'] = $path;
     }
